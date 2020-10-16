a2d75870343f4670b91766fcf4d402b98cd3bfcf
Isaac José Granados Quesada <isaac_granados09@hotmail.com>
2020-10-16 00:50:11 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var app = require('./app');
+var debug = require('debug')('sgabackend:server');
+var http = require('http');
+
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(app.get('port'));
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    debug('Listening on ' + app.get('port'));
+    console.log("Escuchando en port " + app.get('port'));
+}
+
